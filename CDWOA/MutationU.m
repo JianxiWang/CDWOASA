1e0ca4294cc41da67f99f7863b798ae1e2a64ec3
stuipedBoy <85160984+stuipedBoy@users.noreply.github.com>
2021-06-02 21:24:36 +0800

    Delete CDWOA directory

--- CDWOA/MutationU.m
+++
@@ -1,6 +0,0 @@
-function RA=MutationU(dim,Max_iter, inp,Current_iter)
-r=rand(1,dim);
-r=r>=(Current_iter/Max_iter);
-RA=inp;
-%RA(r)=0;%set it to inverted value
-RA(r)=rand(sum(r(:)),1);%set it to new values
